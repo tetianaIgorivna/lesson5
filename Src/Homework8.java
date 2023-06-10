e422560eef289d03406b9543ecb77bfc9ad96f05
tetianaIgorivna <tetiana.ioltukhivska@netcracker.com>
2023-06-10 11:07:35 +0300

    commit 3

--- Src/Homework8.java
+++ Src/Homework8.java
@@ -1,38 +1,26 @@
 import java.util.Scanner;
-import java.util.Collections;
 import java.util.Random;
+
 public class Homework8 {
-    public static void main(String[] args){
-        System.out.print("Спробуйте вгадати загадане комп'ютером число\n");
+    public static void main(String[] args) {
+        System.out.print("Спробуйте вгадати загадане комп'ютером число. Маєте п'ять спроб.");
 
         Random random = new Random();
         int riddledNumber = random.nextInt(10);
+        System.out.print("загадано" + riddledNumber);
 
-        System.out.print("Спроба №1. Введіть ваш варіант: ");
-        Scanner attempt_1 = new Scanner(System.in);
-        int attempt1 = attempt_1.nextInt();
-
-        if (attempt1 == riddledNumber){
-            System.out.print("Ви вгадали!");
-        }else{
-            System.out.print("Ви не вгадали :( \n");
-            System.out.print("Спроба №2. Введіть ваш варіант: ");
-            Scanner attempt_2 = new Scanner(System.in);
-            int attempt2 = attempt_2.nextInt();
-            if (attempt2 == riddledNumber){
+        Scanner attempt = new Scanner(System.in);
+        int attemptNumber = 0;
+        for (int i = 1; i < 6; i++) {
+            System.out.print("\nСпроба №" + i + ". Введіть ваш варіант: ");
+            attemptNumber = attempt.nextInt();
+            if (attemptNumber == riddledNumber) {
                 System.out.print("Ви вгадали!");
-            }else{
+                break;
+            } else {
                 System.out.print("Ви не вгадали :( \n");
-                System.out.print("Спроба №3. Введіть ваш варіант: ");
-                Scanner attempt_3 = new Scanner(System.in);
-                int attempt3 = attempt_3.nextInt();
-                if (attempt3 == riddledNumber){
-                    System.out.print("Ви вгадали!");
-                }else{
-                    System.out.print("Ви не вгадали :( \n");
-                    System.out.print("Всі спроби вичерпано");
-                }
             }
         }
     }
 }
+
