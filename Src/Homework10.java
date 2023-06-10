4addbe353f92a544a847a0d0a3cebb852814592f
tetianaIgorivna <tetiana.ioltukhivska@netcracker.com>
2023-06-10 11:40:31 +0300

    commit 4

--- Src/Homework10.java
+++ Src/Homework10.java
@@ -4,18 +4,34 @@ import java.util.Random;
 public class Homework10 {
     public static void main(String[] args) {
         Random random = new Random();
-        int[] organizerNumbers;
-        organizerNumbers = new int[7];
-        for (int i = 0; i < organizerNumbers.length; i++) {
-            organizerNumbers[i] = random.nextInt(11);
+        int[] team1;
+        team1 = new int[25];
+        for (int i = 0; i < team1.length; i++) {
+            team1[i] = random.nextInt(40 - 18) + 18;
         }
-        System.out.println("Числа фірми-організатора лотереї " + Arrays.toString(organizerNumbers));
+        System.out.println("Вік гравців першої команди " + Arrays.toString(team1));
 
-        int[] playerNumbers;
-        playerNumbers = new int[7];
-        for (int j = 0; j < playerNumbers.length; j++) {
-            playerNumbers[j] = random.nextInt(11);
+        double team1average = 0;
+        int team1sum = 0;
+        for (int i = 0; i < team1.length; i++) {
+            team1sum = team1sum + team1[i];
         }
-        System.out.println("Числа гравця " + Arrays.toString(playerNumbers));
+        team1average = (double) team1sum / team1.length;
+        System.out.println("Середній вік гравців першої команди = " + team1average + "\n");
+
+        int[] team2;
+        team2 = new int[25];
+        for (int i = 0; i < team2.length; i++) {
+            team2[i] = random.nextInt(40 - 18) + 18;
+        }
+        System.out.println("Вік гравців другої команди " + Arrays.toString(team2));
+
+        double team2average = 0;
+        int team2sum = 0;
+        for (int j = 0; j < team1.length; j++) {
+            team2sum = team2sum + team2[j];
+        }
+        team2average = (double) team2sum / team2.length;
+        System.out.println("Середній вік гравців другої команди = " + team2average);
     }
 }
